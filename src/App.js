1f9dfb2c883e36224866c1707640f49edf5057dc
Lucas Oliveira <lucasop888@yahoo.com>
2021-11-11 13:11:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const questions = [
+    {
+      questionText:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit augue quam, non iaculis eros efficitur ut. Sed gravida erat nec lacinia ornare. ",
+      answerOptions: [
+        {
+          answerText: "Verdadeiro",
+          isCorrect: false,
+        },
+        {
+          answerText: "Falso",
+          isCorrect: true,
+        },
+      ],
+    },
+    {
+      questionText:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit augue quam, non iaculis eros efficitur ut. Sed gravida erat nec lacinia ornare. ",
+      answerOptions: [
+        {
+          answerText: "Verdadeiro",
+          isCorrect: false,
+        },
+        {
+          answerText: "Falso",
+          isCorrect: true,
+        },
+      ],
+    },
+    {
+      questionText:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit augue quam, non iaculis eros efficitur ut. Sed gravida erat nec lacinia ornare. ",
+      answerOptions: [
+        {
+          answerText: "Verdadeiro",
+          isCorrect: true,
+        },
+        {
+          answerText: "Falso",
+          isCorrect: false,
+        },
+      ],
+    },
+    {
+      questionText:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit augue quam, non iaculis eros efficitur ut. Sed gravida erat nec lacinia ornare. ",
+      answerOptions: [
+        {
+          answerText: "Verdadeiro",
+          isCorrect: false,
+        },
+
+        {
+          answerText: "Falso",
+          isCorrect: true,
+        },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button
+                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+              >
+                <h4>{answerOption.answerText}</h4>
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
